b1716ff352fb89bd8fb5cea1b5ddb4a0d8763a4e
XiangM7 <157428241+XiangM7@users.noreply.github.com>
2025-01-13 12:09:56 -0800

    Update floor division.cpp

--- floor division.cpp
+++ floor division.cpp
@@ -4,17 +4,14 @@ using namespace std;
 int main() {
     // Declare two integers a and b
     long long a, b;
-    
     // Read the input values
-    cin >> a >> b;
-    
-    // Check if b is 0
-    if (b == 0) {
-        cout << "division by zero!!" << endl;
+    cin>>a>>b;
+    // Check if b is 0 or not
+    if (b==0) {
+        cout << "division by zero!!"<<endl;
     } else {
-        // Calculate and output the floor division result
-        cout << a / b << endl;
+        // Calculate and output
+        cout<<a/b<<endl;
     }
-    
     return 0;
 }
